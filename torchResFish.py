6ab420e66d9a12cbc0c72470c94207fee270dd9c
Eavan Pattie <eavan@lyse.net>
2017-05-12 14:14:50 +0100

    modified loader to work with new directory structure

--- torchResFish.py
+++ torchResFish.py
@@ -18,6 +18,7 @@ plt.ion()
 
 # Constants
 iterations = 1  # Number of training loops
+use_cuda = torch.cuda.is_available()
 
 # Load pretrained ResNet18
 resnet = torchvision.models.resnet18(pretrained=True)
@@ -30,7 +31,9 @@ for param in resnet.parameters():
 resnet.fc = torch.nn.Linear(512,8)
 
 # Load image names
-image_names = {
+sets = ['train', 'test']
+
+image_names = [{
         "OTHER": [],
         "LAG": [],
         "SHARK": [],
@@ -39,15 +42,15 @@ image_names = {
         "ALB": [],
         "YFT": [],
         "DOL": []
-}
+} for x in sets]
 
-for key in image_names.keys():
-    directory_path = "train/" + key
-    image_names[key] = os.listdir(directory_path)
-
+for i, x in enumerate(sets):
+    for key in image_names[i].keys():
+        directory_path = x + "/" + key
+        image_names[i][key] = os.listdir(directory_path)
 
 # Image loader
-def load_images(shape, one_hot_encoding=True):  # Shape should be (batch, channels, height, width)
+def load_images(shape, dataset="train", one_hot_encoding=True):  # Shape should be (batch, channels, height, width)
     
     # Check that four dimensions have been given
     assert len(shape) is 4
@@ -56,24 +59,35 @@ def load_images(shape, one_hot_encoding=True):  # Shape should be (batch, channe
     images_list = []
     labels_list = []
 
+    if dataset is "train":
+        index = 0
+    elif dataset is "test":
+        index = 1
+    else:
+        print("Invalid parameter for load images: " + dataset)
+
     for i in range(shape[0]):  # Loop over the size of the batch_size
 
         # Pick a random index from the image types
-        type_index = int(random.random() * len(image_names))
+        type_index = int(random.random() * len(image_names[index]))
 
         # Get the abrv. name of fish type
-        fish_type = list(image_names.keys())[type_index]
+        fish_type = list(image_names[index].keys())[type_index]
 
         # Pick random image from dataset with given label
-        image_index = int(random.random() * len(image_names[fish_type]))
+        image_index = int(random.random() * len(image_names[index][fish_type]))
+        print("Image index is {} out of {}".format(image_index, 
+            len(image_names[index][fish_type])))
 
         # Load the image and resize it to be the given shape
-        raw_image = scipy.ndimage.imread("train/" + fish_type + "/" + image_names[fish_type][image_index])
+        raw_image = scipy.ndimage.imread(
+                dataset + "/" + fish_type + "/" + 
+                image_names[index][fish_type][image_index])
         resized_image = scipy.misc.imresize(raw_image, (shape[1], shape[2]))
 
         # Convert label to one hot encoding
         if one_hot_encoding:
-            one_hot_vector = np.zeros((len(image_names)))
+            one_hot_vector = np.zeros((len(image_names[index])))
             one_hot_vector[type_index] = 1
             labels_list.append(one_hot_vector)
         else:
@@ -88,7 +102,7 @@ def load_images(shape, one_hot_encoding=True):  # Shape should be (batch, channe
 for iter in tqdm(range(iterations)):
 
     # Load images and labels
-    imgs, labels = load_images((64, 224, 224, 3))
+    imgs, labels = load_images((64, 224, 224, 3), dataset="train")
     plt.imshow(imgs[0])
     plt.pause(1)
     imgs = np.transpose(imgs, (0,3,1,2))
@@ -97,3 +111,4 @@ for iter in tqdm(range(iterations)):
     print(imgs[0])
     print(imgs[0].data.type())
     # print(resnet(imgs.float()))  # Not enough ram to do this.. :'(
+
